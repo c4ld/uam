89e6845ba1d06cb4e2186efeee808ca557b182df
c4ld <c4ld@proton.me>
2024-10-03 18:37:24 -0700

    Add files via upload

---
+++ 2nd Semester/Week 7/Binario/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+string rutaArchivo = "archivo.bin";
+string cadenaBinaria = File.ReadAllText(rutaArchivo).Trim();
+cadenaBinaria = cadenaBinaria.Replace(" ", "");
+
+string textoConvertido = ConvertirBinarioATexto(cadenaBinaria);
+Console.WriteLine("Texto convertido:");
+Console.WriteLine(textoConvertido);
+
+static string ConvertirBinarioATexto(string cadenaBinaria)
+{
+    string resultado = "";
+    for (int i = 0; i < cadenaBinaria.Length; i += 8)
+    {
+        if (i + 8 <= cadenaBinaria.Length)
+        {
+            string byteBinario = cadenaBinaria.Substring(i, 8);
+            resultado += (char)Convert.ToByte(byteBinario, 2);
+        }
+    }
+    return resultado;
+}
