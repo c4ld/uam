bdc3af3a502732e0c0be86e7124063cca9edab30
c4ld <c4ld@proton.me>
2024-10-03 18:25:29 -0700

    Add files via upload

---
+++ 2nd Semester/Week 7/Ejemplo/Ejemplo/Program.cs
@@ -0,0 +1,13 @@
+﻿string ruta = "archivo.bin";
+string escrito = "¡Adiós, mundo!";
+
+try
+{
+    string contenido = File.ReadAllText(ruta);
+    Console.WriteLine(contenido);
+    File.WriteAllText(ruta, escrito);
+}
+catch (FileNotFoundException e)
+{
+    Console.WriteLine("Archivo no encontrado: " + e.Message);
+}
