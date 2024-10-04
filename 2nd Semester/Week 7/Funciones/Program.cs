dcc3961e07ba8567483bbf45fbe8f2bc6f200eba
c4ld <c4ld@proton.me>
2024-10-03 19:00:28 -0700

    Add files via upload

---
+++ 2nd Semester/Week 7/Funciones/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualBasic.FileIO;
+
+string archivo = "binario.bin";
+
+string mensaje;
+int cif = 23020134;
+
+using (BinaryWriter Escritor = new BinaryWriter(File.Open(archivo, FileMode.Append)))
+{
+    Escritor.Write(cif);
+    Escritor.Write($"Su CIF es {cif}");
+}
+
+using (BinaryReader Lector = new BinaryReader(File.Open(archivo, FileMode.Open)))
+{
+    mensaje = Lector.ReadString();
+}
+Console.WriteLine(mensaje);
